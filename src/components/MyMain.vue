c5382d624fbd9d9ea24bb339979505777c2d9824
mouhcine <mouhcine15.00@gmail.com>
2022-06-10 15:20:59 +0200

    partenza

---
+++ src/components/MyMain.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        mouhcine
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MyMain',
+  props: {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped >
+    div {
+        background-color: black;
+    }
+</style>
